ddf3117327f44a022a9468898b3a887580de27ed
jherve <julien.jev.herve@gmail.com>
2024-01-30 10:44:31 +0100

    Adapt Output to new Extractible class

--- src/LinkedIn/Output.purs
+++ src/LinkedIn/Output.purs
@@ -1,11 +1,9 @@
 module LinkedIn.Output where
 
-import Prelude
-
 import Data.Either (Either)
 import Effect (Effect)
 import LinkedIn (run)
-import LinkedIn.Output.Types (Output(..))
+import LinkedIn.Output.Types (Output)
 import LinkedIn.Page.JobOffer (JobOfferPage)
 import LinkedIn.Page.Projects (ProjectsPage)
 import LinkedIn.Page.Skills (SkillsPage)
@@ -16,7 +14,7 @@ import Web.DOM (Document)
 
 toPage ∷ Partial ⇒ PageUrl → Document → Effect (Either String Output)
 toPage ctx dom = case ctx of
-  UrlProjects _ -> map (map OutProjects) $ run (Proxy :: Proxy ProjectsPage) dom
-  UrlSkills _ ->  map (map OutSkills) $ run (Proxy :: Proxy SkillsPage) dom
-  UrlWorkExperience _ ->  map (map OutWorkExperiences) $ run (Proxy :: Proxy WorkExperiencesPage) dom
-  UrlJobOffer _ -> map (map OutJobOffer) $ run (Proxy :: Proxy JobOfferPage) dom
+  UrlProjects _ -> run (Proxy :: Proxy ProjectsPage) dom
+  UrlSkills _ -> run (Proxy :: Proxy SkillsPage) dom
+  UrlWorkExperience _ -> run (Proxy :: Proxy WorkExperiencesPage) dom
+  UrlJobOffer _ -> run (Proxy :: Proxy JobOfferPage) dom

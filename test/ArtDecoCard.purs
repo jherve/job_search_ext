1cfb5263d72a60963f77a14a6c4036a8d1711f37
jherve <julien.jev.herve@gmail.com>
2024-02-04 18:44:57 +0100

    Move test reference values to a record

--- test/ArtDecoCard.purs
+++ test/ArtDecoCard.purs
@@ -21,49 +21,57 @@ import Test.Spec.Assertions (fail, shouldEqual)
 import Test.Utils (detachFromFile, getOutputFromFile, toMonthYear')
 import Type.Proxy (Proxy(..))
 
+type TestCase a = { detached ∷ a DetachedNode , filePath ∷ String , output ∷ WorkExperience }
+
+andrewNgWorkExperience ∷ TestCase ArtDecoCardElement
+andrewNgWorkExperience = {
+  filePath: "test/examples/andrew_ng_experiences.html",
+  detached: ArtDecoCardElement {
+    pvs_entity: (ArtDecoPvsEntity {
+      center: (ArtDecoCenter {
+        content: (ArtDecoCenterContent (NonEmptyList (NonEmpty (ArtDecoPvsEntitySubComponent (
+          DetachedElement {classes: Nil, content: "DeepLearning.AI provides\ntechnical training on Generative AI, Machine Learning, Deep Learning,\nand other topics. We also offer a widely read newsletter, The Batch\n(thebatch.ai), that covers what matters in AI right now. Our courses are often created with industry-leading AI companies (AWS,\nGoogle, OpenAI, etc.), and we offer both short courses that can be\ncompleted in an hour, and longer courses and specializations hosted on\nCoursera that give you a solid foundation in some aspect of AI. These\ncourses are designed to offer hands-on practice with AI technologies,\nand you will gain practical, job-ready skills. Whether you are just starting out in AI or seeking to further an existing\ncareer, come see if we can help, at http://deeplearning.ai!", id: Nothing, tag: "SPAN"}))
+          Nil
+        ))),
+        header: (ArtDecoCenterHeader {
+          bold: (DetachedElement {classes: Nil, content: "Founder", id: Nothing, tag: "SPAN" }),
+          light: (Just (NonEmptyList (NonEmpty (
+            DetachedElement {classes: ("pvs-entity__caption-wrapper" : Nil), content: "juin 2017 - aujourd’hui · 6 ans 7 mois", id: Nothing,tag: "SPAN"})
+            ((DetachedElement {classes: Nil, content: "Palo Alto, California, United States", id: Nothing,tag: "SPAN"}) : Nil)
+          ))),
+          normal: (Just (DetachedElement {classes: Nil, content: "DeepLearning.AI", id: Nothing, tag: "SPAN" }))
+        })
+      }),
+      side: unit
+    })
+  },
+  output: WorkExperience {
+    company: Just "DeepLearning.AI",
+    contractType: Nothing,
+    description: Just "DeepLearning.AI provides\ntechnical training on Generative AI, Machine Learning, Deep Learning,\nand other topics. We also offer a widely read newsletter, The Batch\n(thebatch.ai), that covers what matters in AI right now. Our courses are often created with industry-leading AI companies (AWS,\nGoogle, OpenAI, etc.), and we offer both short courses that can be\ncompleted in an hour, and longer courses and specializations hosted on\nCoursera that give you a solid foundation in some aspect of AI. These\ncourses are designed to offer hands-on practice with AI technologies,\nand you will gain practical, job-ready skills. Whether you are just starting out in AI or seeking to further an existing\ncareer, come see if we can help, at http://deeplearning.ai!",
+    duration: Just (YearsMonth 6 7),
+    position: "Founder",
+    timeSpan: Just (TimeSpanToToday (toMonthYear' June 2017))
+  }
+}
+
 artDecoCardsSpec :: Spec Unit
 artDecoCardsSpec = do
   describe "Art deco cards parsing" do
     it "works" do
-      cards <- detachFromFile (Proxy :: Proxy WorkExperiencesPage) "test/examples/andrew_ng_experiences.html"
+      cards <- detachFromFile (Proxy :: Proxy WorkExperiencesPage) andrewNgWorkExperience.filePath
 
       case cards of
         Left _ -> fail "Detach operation failed"
         Right (WorkExperiencesPage c) -> do
           let head = NEL.head c
-
-          head `shouldEqual` ArtDecoCardElement {
-            pvs_entity: (ArtDecoPvsEntity {
-              center: (ArtDecoCenter {
-                content: (ArtDecoCenterContent (NonEmptyList (NonEmpty (ArtDecoPvsEntitySubComponent (
-                  DetachedElement {classes: Nil, content: "DeepLearning.AI provides\ntechnical training on Generative AI, Machine Learning, Deep Learning,\nand other topics. We also offer a widely read newsletter, The Batch\n(thebatch.ai), that covers what matters in AI right now. Our courses are often created with industry-leading AI companies (AWS,\nGoogle, OpenAI, etc.), and we offer both short courses that can be\ncompleted in an hour, and longer courses and specializations hosted on\nCoursera that give you a solid foundation in some aspect of AI. These\ncourses are designed to offer hands-on practice with AI technologies,\nand you will gain practical, job-ready skills. Whether you are just starting out in AI or seeking to further an existing\ncareer, come see if we can help, at http://deeplearning.ai!", id: Nothing, tag: "SPAN"}))
-                  Nil
-                ))),
-                header: (ArtDecoCenterHeader {
-                  bold: (DetachedElement {classes: Nil, content: "Founder", id: Nothing, tag: "SPAN" }),
-                  light: (Just (NonEmptyList (NonEmpty (
-                    DetachedElement {classes: ("pvs-entity__caption-wrapper" : Nil), content: "juin 2017 - aujourd’hui · 6 ans 7 mois", id: Nothing,tag: "SPAN"})
-                    ((DetachedElement {classes: Nil, content: "Palo Alto, California, United States", id: Nothing,tag: "SPAN"}) : Nil)
-                  ))),
-                  normal: (Just (DetachedElement {classes: Nil, content: "DeepLearning.AI", id: Nothing, tag: "SPAN" }))
-                })
-              }),
-              side: unit
-            })
-          }
+          head `shouldEqual` andrewNgWorkExperience.detached
 
     it "reads the work experience" do
-      wxpPage <- getOutputFromFile (Proxy :: Proxy WorkExperiencesPage) "test/examples/andrew_ng_experiences.html"
+      wxpPage <- getOutputFromFile (Proxy :: Proxy WorkExperiencesPage) andrewNgWorkExperience.filePath
 
       case wxpPage of
         Right (OutWorkExperiences weps) -> do
           let head = NEL.head weps
-          head `shouldEqual` WorkExperience {
-            company: Just "DeepLearning.AI",
-            contractType: Nothing,
-            description: Just "DeepLearning.AI provides\ntechnical training on Generative AI, Machine Learning, Deep Learning,\nand other topics. We also offer a widely read newsletter, The Batch\n(thebatch.ai), that covers what matters in AI right now. Our courses are often created with industry-leading AI companies (AWS,\nGoogle, OpenAI, etc.), and we offer both short courses that can be\ncompleted in an hour, and longer courses and specializations hosted on\nCoursera that give you a solid foundation in some aspect of AI. These\ncourses are designed to offer hands-on practice with AI technologies,\nand you will gain practical, job-ready skills. Whether you are just starting out in AI or seeking to further an existing\ncareer, come see if we can help, at http://deeplearning.ai!",
-            duration: Just (YearsMonth 6 7),
-            position: "Founder",
-            timeSpan: Just (TimeSpanToToday (toMonthYear' June 2017))
-          }
+          head `shouldEqual` andrewNgWorkExperience.output
         _ -> fail "Test failed"
--- test/JobsUnifiedTopCard.purs
+++ test/JobsUnifiedTopCard.purs
@@ -18,84 +18,91 @@ import Test.Spec.Assertions (shouldEqual)
 import Test.Utils (detachFromFile, getOutputFromFile)
 import Type.Proxy (Proxy(..))
 
+type TestCase a = { detached ∷ a DetachedNode , filePath ∷ String , output ∷ Output }
+
+jobOfferPage_3786945580 ∷ TestCase JobOfferPage
+jobOfferPage_3786945580 = {
+  filePath: "test/examples/job_offer_3786945580.html",
+  detached: JobOfferPage (JobsUnifiedTopCardElement {
+    actions: (Just (NonEmptyList (NonEmpty (TopCardActionButton
+      (DetachedButton {
+        classes: ("jobs-apply-button" : "artdeco-button" : "artdeco-button--3" : "artdeco-button--primary" : "ember-view" : Nil),
+        content: "Candidature simplifiée",
+        role: Nothing
+      }))
+      ((TopCardActionButton (DetachedButton {
+        classes: ("jobs-save-button" : "artdeco-button" : "artdeco-button--3" : "artdeco-button--secondary" : Nil),
+        content: "Enregistrer Enregistrer Data Engineer H/F - Secteur Energie chez LINCOLN",
+        role: Nothing
+      })) : Nil)
+    ))),
+    header: (DetachedElement {
+      classes: ("t-24" : "t-bold" : "job-details-jobs-unified-top-card__job-title" : Nil),
+      content: "Data Engineer H/F - Secteur Energie",
+      id: Nothing,
+      tag: "H1"
+    }),
+    insights: (Just (NonEmptyList (NonEmpty (TopCardInsight {
+        content: (TopCardInsightContentSecondary {
+          primary: (DetachedElement {classes: Nil, content: "Sur site", id: Nothing, tag: "SPAN"}),
+          secondary: (NonEmptyList (NonEmpty (TopCardSecondaryInsightNested
+            (DetachedElement {classes: Nil, content: "Temps plein", id: Nothing, tag: "SPAN"}))
+            ((TopCardSecondaryInsightPlain
+            (DetachedElement {classes: ("job-details-jobs-unified-top-card__job-insight-view-model-secondary" : Nil), content: "Confirmé", id: Nothing,tag: "SPAN" })) : Nil))
+          )
+        }),
+        icon: DetachedLiIcon "job"
+      }) ((TopCardInsight {
+          content: (TopCardInsightContentSingle (
+            DetachedElement {classes: Nil, content: "201-500 employés · Technologies et services de l’information", id: Nothing, tag: "SPAN" }
+          )),
+          icon: DetachedLiIcon "company"
+        }) : (TopCardInsight {
+            content: (TopCardInsightContentSingle (
+              DetachedElement {classes: Nil, content: "2 anciens élèves travaillent ici", id: Nothing, tag: "SPAN" }
+            )),
+            icon: DetachedLiIcon "people"
+            }) : (TopCardInsight {
+              content: (TopCardInsightContentSingle (
+                DetachedElement {classes: Nil, content: "Découvrez comment vous vous positionnez par rapport à 87 candidats. Essai Premium pour 0 EUR", id: Nothing, tag: "SPAN" })),
+              icon: (DetachedSvgElement { dataTestIcon: (Just "lightbulb-medium"), id: Nothing, tag: "svg" })
+            }) : (TopCardInsight {
+              content: (TopCardInsightContentButton (
+                DetachedButton {classes: ("job-details-jobs-unified-top-card__job-insight-text-button" : Nil), content: "9 compétences sur 11 correspondent à votre profil, vous pourriez bien convenir pour ce poste", role: Nothing}
+              )),
+              icon: (DetachedSvgElement { dataTestIcon: (Just "checklist-medium"), id: Nothing, tag: "svg" })
+            }) : Nil)))),
+    primaryDescription: (TopCardPrimaryDescription {
+      link: (DetachedA { content: "LINCOLN", href: "https://www.linkedin.com/company/lincoln-/life" }),
+      text: (DetachedText "· Boulogne-Billancourt, Île-de-France, France"),
+      tvmText: (Just (NonEmptyList (NonEmpty (
+        DetachedElement {classes: ("tvm__text" : "tvm__text--neutral" : Nil), content: "il y a 2 semaines", id: Nothing, tag: "SPAN"})
+        ((DetachedElement {classes: ("tvm__text" : "tvm__text--neutral" : Nil), content: "·", id: Nothing, tag: "SPAN"})
+          : (DetachedElement {classes: ("tvm__text" : "tvm__text--neutral" : Nil), content: "87 candidats", id: Nothing, tag: "SPAN"})
+          : Nil
+        ))
+      ))
+    })
+  }),
+  output: OutJobOffer (JobOffer {
+    companyDomain: (Just "Technologies et services de l’information"),
+    companyLink: "https://www.linkedin.com/company/lincoln-/life",
+    companyName: "LINCOLN",
+    companySize: (Just "201-500 employés"),
+    hasSimplifiedApplicationProcess: true,
+    location: (Just "Boulogne-Billancourt, Île-de-France, France"),
+    flexibility: (Just JobFlexOnSite),
+    title: "Data Engineer H/F - Secteur Energie"
+  })
+}
+
 jobsUnifiedTopCardSpec :: Spec Unit
 jobsUnifiedTopCardSpec = do
   describe "Jobs top card parsing" do
     it "reads well as a JobOfferPage DetachedNode" do
-      topCard <- detachFromFile (Proxy :: Proxy JobOfferPage) "test/examples/job_offer.html"
-
-      topCard `shouldEqual` Right(JobOfferPage (JobsUnifiedTopCardElement {
-        actions: (Just (NonEmptyList (NonEmpty (TopCardActionButton
-          (DetachedButton {
-            classes: ("jobs-apply-button" : "artdeco-button" : "artdeco-button--3" : "artdeco-button--primary" : "ember-view" : Nil),
-            content: "Candidature simplifiée",
-            role: Nothing
-          }))
-          ((TopCardActionButton (DetachedButton {
-            classes: ("jobs-save-button" : "artdeco-button" : "artdeco-button--3" : "artdeco-button--secondary" : Nil),
-            content: "Enregistrer Enregistrer Data Engineer H/F - Secteur Energie chez LINCOLN",
-            role: Nothing
-          })) : Nil)
-        ))),
-        header: (DetachedElement {
-          classes: ("t-24" : "t-bold" : "job-details-jobs-unified-top-card__job-title" : Nil),
-          content: "Data Engineer H/F - Secteur Energie",
-          id: Nothing,
-          tag: "H1"
-        }),
-        insights: (Just (NonEmptyList (NonEmpty (TopCardInsight {
-            content: (TopCardInsightContentSecondary {
-              primary: (DetachedElement {classes: Nil, content: "Sur site", id: Nothing, tag: "SPAN"}),
-              secondary: (NonEmptyList (NonEmpty (TopCardSecondaryInsightNested
-                (DetachedElement {classes: Nil, content: "Temps plein", id: Nothing, tag: "SPAN"}))
-                ((TopCardSecondaryInsightPlain
-                (DetachedElement {classes: ("job-details-jobs-unified-top-card__job-insight-view-model-secondary" : Nil), content: "Confirmé", id: Nothing,tag: "SPAN" })) : Nil))
-              )
-            }),
-            icon: DetachedLiIcon "job"
-          }) ((TopCardInsight {
-              content: (TopCardInsightContentSingle (
-                DetachedElement {classes: Nil, content: "201-500 employés · Technologies et services de l’information", id: Nothing, tag: "SPAN" }
-              )),
-              icon: DetachedLiIcon "company"
-            }) : (TopCardInsight {
-                content: (TopCardInsightContentSingle (
-                  DetachedElement {classes: Nil, content: "2 anciens élèves travaillent ici", id: Nothing, tag: "SPAN" }
-                )),
-                icon: DetachedLiIcon "people"
-                }) : (TopCardInsight {
-                  content: (TopCardInsightContentSingle (
-                    DetachedElement {classes: Nil, content: "Découvrez comment vous vous positionnez par rapport à 87 candidats. Essai Premium pour 0 EUR", id: Nothing, tag: "SPAN" })),
-                  icon: (DetachedSvgElement { dataTestIcon: (Just "lightbulb-medium"), id: Nothing, tag: "svg" })
-                }) : (TopCardInsight {
-                  content: (TopCardInsightContentButton (
-                    DetachedButton {classes: ("job-details-jobs-unified-top-card__job-insight-text-button" : Nil), content: "9 compétences sur 11 correspondent à votre profil, vous pourriez bien convenir pour ce poste", role: Nothing}
-                  )),
-                  icon: (DetachedSvgElement { dataTestIcon: (Just "checklist-medium"), id: Nothing, tag: "svg" })
-                }) : Nil)))),
-        primaryDescription: (TopCardPrimaryDescription {
-          link: (DetachedA { content: "LINCOLN", href: "https://www.linkedin.com/company/lincoln-/life" }),
-          text: (DetachedText "· Boulogne-Billancourt, Île-de-France, France"),
-          tvmText: (Just (NonEmptyList (NonEmpty (
-            DetachedElement {classes: ("tvm__text" : "tvm__text--neutral" : Nil), content: "il y a 2 semaines", id: Nothing, tag: "SPAN"})
-            ((DetachedElement {classes: ("tvm__text" : "tvm__text--neutral" : Nil), content: "·", id: Nothing, tag: "SPAN"})
-              : (DetachedElement {classes: ("tvm__text" : "tvm__text--neutral" : Nil), content: "87 candidats", id: Nothing, tag: "SPAN"})
-              : Nil
-            ))
-          ))
-        })
-      }))
+      topCard <- detachFromFile (Proxy :: Proxy JobOfferPage) jobOfferPage_3786945580.filePath
+      topCard `shouldEqual` Right(jobOfferPage_3786945580.detached)
 
     it "reads the JobOffer" do
-      jobOffer <- getOutputFromFile (Proxy :: Proxy JobOfferPage) "test/examples/job_offer.html"
-
-      jobOffer `shouldEqual` Right (OutJobOffer (JobOffer {
-        companyDomain: (Just "Technologies et services de l’information"),
-        companyLink: "https://www.linkedin.com/company/lincoln-/life",
-        companyName: "LINCOLN",
-        companySize: (Just "201-500 employés"),
-        hasSimplifiedApplicationProcess: true,
-        location: (Just "Boulogne-Billancourt, Île-de-France, France"),
-        flexibility: (Just JobFlexOnSite),
-        title: "Data Engineer H/F - Secteur Energie"
-      }))
+      jobOffer <- getOutputFromFile (Proxy :: Proxy JobOfferPage) jobOfferPage_3786945580.filePath
+      jobOffer `shouldEqual` Right (jobOfferPage_3786945580.output)

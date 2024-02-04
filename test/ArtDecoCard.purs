fc72c2b783ecca993ff16ce543c27dc192e178ee
jherve <julien.jev.herve@gmail.com>
2024-02-04 16:11:01 +0100

    Remove some bad test code

--- test/ArtDecoCard.purs
+++ test/ArtDecoCard.purs
@@ -13,13 +13,12 @@ import LinkedIn.DetachedNode (DetachedNode(..))
 import LinkedIn.Output.Types (Output(..))
 import LinkedIn.Page.WorkExperiences (WorkExperiencesPage(..))
 import LinkedIn.Profile.WorkExperience (WorkExperience(..))
-import LinkedIn.Profile.WorkExperience as PWE
 import LinkedIn.UI.Basic.Types (Duration(..), TimeSpan(..))
 import LinkedIn.UI.Components.ArtDeco (ArtDecoCenter(..), ArtDecoCenterContent(..), ArtDecoCenterHeader(..), ArtDecoPvsEntity(..), ArtDecoPvsEntitySubComponent(..))
 import LinkedIn.UI.Components.ArtDecoCard (ArtDecoCardElement(..))
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (fail, shouldEqual)
-import Test.Utils (detachFromFile, fromDetachedToUI, getOutputFromFile, toMonthYear')
+import Test.Utils (detachFromFile, getOutputFromFile, toMonthYear')
 import Type.Proxy (Proxy(..))
 
 artDecoCardsSpec :: Spec Unit
@@ -73,19 +72,6 @@ artDecoCardsSpec = do
               side: unit
             })
           }
-
-          let
-            jobOffer = (PWE.fromUI <=< fromDetachedToUI) head
-
-          jobOffer `shouldEqual` Right (WorkExperience {
-            company: Just "DeepLearning.AI",
-            contractType: Nothing,
-            description: Just "DeepLearning.AI provides\ntechnical training on Generative AI, Machine Learning, Deep Learning,\nand other topics. We also offer a widely read newsletter, The Batch\n(thebatch.ai), that covers what matters in AI right now. Our courses are often created with industry-leading AI companies (AWS,\nGoogle, OpenAI, etc.), and we offer both short courses that can be\ncompleted in an hour, and longer courses and specializations hosted on\nCoursera that give you a solid foundation in some aspect of AI. These\ncourses are designed to offer hands-on practice with AI technologies,\nand you will gain practical, job-ready skills. Whether you are just starting out in AI or seeking to further an existing\ncareer, come see if we can help, at http://deeplearning.ai!",
-            duration: Just (YearsMonth 6 7),
-            position: "Founder",
-            timeSpan: Just (TimeSpanToToday (toMonthYear' June 2017))
-          })
-
 
     it "reads the work experience" do
       wxpPage <- getOutputFromFile (Proxy :: Proxy WorkExperiencesPage) "test/examples/andrew_ng_experiences.html"

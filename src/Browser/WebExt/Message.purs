c29efa80ba6f29974f336114c46be7c37016ec88
jherve <julien.jev.herve@gmail.com>
2024-02-11 18:28:45 +0100

    Add unwrapMessage

--- src/Browser/WebExt/Message.purs
+++ src/Browser/WebExt/Message.purs
@@ -1,7 +1,9 @@
-module Browser.WebExt.Message (Message, mkMessage, displayMessage) where
+module Browser.WebExt.Message (Message, mkMessage, displayMessage, unwrapMessage) where
 
 import Data.Argonaut.Core (Json, stringifyWithIndent)
+import Data.Argonaut.Decode (class DecodeJson, JsonDecodeError, decodeJson)
 import Data.Argonaut.Encode (class EncodeJson, encodeJson)
+import Data.Either (Either)
 
 type Message = Json
 
@@ -10,3 +12,6 @@ mkMessage = encodeJson
 
 displayMessage ∷ Message → String
 displayMessage = stringifyWithIndent 2
+
+unwrapMessage ∷ ∀ (@a ∷ Type). DecodeJson a ⇒ Json → Either JsonDecodeError a
+unwrapMessage = decodeJson
